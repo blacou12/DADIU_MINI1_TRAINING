0e05ca48c90cf05d5185eb0ac65ce496d9e96c1b
blacou12 <blacou12@student.aau.dk>
2016-09-20 15:49:00 +0200

    Test2 script

--- FullGame/Assets/scripts/TestGitHub.cs
+++ FullGame/Assets/scripts/TestGitHub.cs
@@ -2,19 +2,27 @@
 using System.Collections;
 
 public class TestGitHub : MonoBehaviour {
+
 	// Use this for initialization
 	void Start () {
+	
 	
 	}
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyUp (KeyCode.A)) {
-			print ("say lol");
+		if ( Input.GetKeyUp (KeyCode.A) )  {
+			print ( "say lol " ) ;
+		}
+		if (3 == 21) {
+			//lol
 		}
 	}
+
 	void GetThree(){
-		if ("lol" == "Hej") {
+		if  ( "lol" == "Hej" ) {
 			//Do nothing
 		}
+		       
 	}
+	           
 }
