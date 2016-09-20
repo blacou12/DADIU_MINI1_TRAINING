1079d299a87e5bc379ce31aeb088b4ae172a9b98
blacou12 <blacou12@student.aau.dk>
2016-09-20 15:45:46 +0200

    Nyt

---
+++ FullGame/Assets/scripts/testgithubtwo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testgithubtwo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (1 == 2) {
+			print ("twest");
+		}
+	}
+}
